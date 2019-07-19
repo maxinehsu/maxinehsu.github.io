3d0f1ad07598c3a1cb98dc7423872fd029cfdcae
maxinehsu <53087364+maxinehsu@users.noreply.github.com>
2019-07-19 09:41:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// alert("Hello?")
+
+//change size of image
+function imgSize(x)
+{
+  if (x.style.width == "300px" && x.style.height == "200px")
+  {
+    x.style.width = "1000px";
+    x.style.height = "666px";
+  }
+  else if (x.style.width == "1000px" && x.style.height == "666px")
+  {
+    x.style.width = "300px";
+    x.style.height = "200px";
+  } //end if
+} //end of imgSize function
